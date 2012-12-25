9f39c96408e69268197dbb306d0e8d55484f7afb
Jukka Svahn <web@rahinaa.biz>
2012-12-25 10:29:03 +0200

    Missing comma.

--- src/Netcarver/Textile/Parser.php
+++ src/Netcarver/Textile/Parser.php
@@ -1468,7 +1468,7 @@ class Parser
      * Determines the list type from the textile input symbol.
      *
      * @param  string $in Textile input containing the possible list marker
-     * @return string Either 'd', 'o' 'u'
+     * @return string Either 'd', 'o', 'u'
      */
     protected function liType($in)
     {
